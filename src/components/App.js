e46794b2c38fabdc2279a8a37c092bd227c81ad8
Kairo20 <jjangg0301@naver.com>
2022-08-01 08:59:43 +0900

    implemented login

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
-import AppRouter from "./Router";
+import AppRouter from "components/Router";
+import React, { useState } from "react";
+import { authService } from "myBase";
 
 function App() {
-  return <AppRouter />;
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer> &copy; Nwitter {new Date().getFullYear()}</footer>
+    </>
+  );
 }
 
 export default App;
